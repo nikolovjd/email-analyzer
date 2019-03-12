95f254390df083043549d4c5cf80655c1a31532f
Jordán Nikolov <nikolov.jd@gmail.com>
2019-03-12 18:32:12 +0100

    Add db to project

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -4,13 +4,17 @@ import { createConnection, getConnection } from 'typeorm';
 @injectable()
 class DatabaseService {
   private readonly initialized: Promise<any>;
+  private readonly config: any;
   constructor() {
+    // TODO: find a better way. Seems like TypeORM is trying to assign to a param in config, which is deepFrozen
+    this.config = Object.assign({}, config.db);
     this.initialized = this.init();
   }
 
   public async init() {
     try {
-      await createConnection(config.db);
+      await createConnection(this.config);
+      console.log('DBS initialized');
     } catch (err) {
       console.log('Cannot Initialize DatabaseService', err);
       process.exit(2);
